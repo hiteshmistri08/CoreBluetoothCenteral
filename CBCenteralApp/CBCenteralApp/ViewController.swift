eee6c270b7f810f6c89db78996983d3fdd7485d9
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2024-12-17 21:58:11 +0530

    - Add Bluetooth privacy - Add Central manager

--- CBCenteralApp/CBCenteralApp/ViewController.swift
+++ CBCenteralApp/CBCenteralApp/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    private var manager: CBCentralManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCenteralManager()
     }
 
-
+    func setupCenteralManager() {
+        manager = CBCentralManager(delegate: self, queue: nil)
+    }
 }
 
+extension ViewController {
+    func alert(msg: String) {
+        let vc = UIAlertController(title: "Alert", message: msg, preferredStyle: .alert)
+        self.present(vc, animated: true)
+    }
+}
+
+// - MARK: - CBCentralManagerDelegate
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            break
+        case .resetting:
+            alert(msg: "Bluetooth is resetting")
+            break
+        case .unsupported:
+            alert(msg: "Bluetooth is not supported")
+            break
+        case .unauthorized:
+            break
+        case .poweredOff:
+            debugPrint("Bluetooth power is off")
+            alert(msg: "Bluetooth is off. Please turn on the bluetooth")
+            break
+        case .poweredOn:
+            debugPrint("Bluetooth power is on")
+            break
+        @unknown default:
+            break
+        }
+    }
+}
